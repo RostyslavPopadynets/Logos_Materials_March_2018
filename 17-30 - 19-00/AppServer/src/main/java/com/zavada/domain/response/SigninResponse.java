e964c323e1f77ede66ce8182eb3d6a84b0d90777
VolodymyrZavada <volodymyrzavada@gmail.com>
2018-09-05 21:32:13 +0300

    05.09

---
+++ 17-30 - 19-00/AppServer/src/main/java/com/zavada/domain/response/SigninResponse.java
@@ -0,0 +1,14 @@
+package com.zavada.domain.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SigninResponse {
+
+	private String token;
+
+}
